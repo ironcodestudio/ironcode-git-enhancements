44195dd6977a62abfc28a8de82b41af7650e3169
Sal Ferrarello <salcode@salogic.net>
2013-11-19 13:50:06 -0500

    added file with bash modifications

---
+++ bash_profile-mods.bash
@@ -0,0 +1,30 @@
+echo 'bash_profile-mods.bash';
+# moved /usr/local/git/bin to front of path to use custom git (not Xcode version)
+export PATH=/usr/local/git/bin:$PATH
+
+###
+# ENABLE git tab completion
+###
+
+# if the git-completion.bash script exists, load it
+if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
+    . /usr/local/git/contrib/completion/git-completion.bash
+fi
+
+###
+# ENABLE git status in prompt
+###
+
+# if the git-prompt.sh script exists,
+if [ -f /usr/local/git/contrib/completion/git-prompt.sh ]; then
+
+    # load prompt script
+    . /usr/local/git/contrib/completion/git-prompt.sh
+
+    # ??
+    GIT_PS1_SHOWDIRTYSTATE=true
+
+    # update prompt to include git status __git_ps1
+    export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
+
+fi
