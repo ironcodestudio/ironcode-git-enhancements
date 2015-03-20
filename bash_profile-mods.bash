5ef1ccecaa40039ce5df381aaa1457c5fe70fa54
Sal Ferrarello <salcode@salogic.net>
2015-03-20 09:48:02 -0400

    Wrap gbv alias parameters in quotes
    Fixes #5

--- bash_profile-mods.bash
+++ bash_profile-mods.bash
@@ -69,7 +69,7 @@ alias gb='git branch'
 __git_shortcut  gb    branch
 
 alias gbv='git branch -a -v' # shows both local and remote branches AND verbose
-__git_shortcut  gbv   branch -a -v
+__git_shortcut  gbv   branch "-a -v"
 
 alias gc='git commit'
 __git_shortcut  gc    commit
