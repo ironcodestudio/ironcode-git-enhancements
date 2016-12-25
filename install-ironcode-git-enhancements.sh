22a25b0f8a6aa01214f44ae935ae886f72f387a3
Sal Ferrarello <sal@ironcodestudio.com>
2016-12-25 16:24:52 -0500

    Add new lines when writing to configuration files on install
    Add new lines before and after the content we add to:
    
    - `~/.bash_profile`
    - `~/.gitconfig`
    
    See #31

--- install-ironcode-git-enhancements.sh
+++ install-ironcode-git-enhancements.sh
@@ -6,7 +6,7 @@
 git clone https://github.com/ironcodestudio/ironcode-git-enhancements.git ~/ironcode-git-enhancements
 
 # Add line to ~/.bash_profile to load our Git modifications
-printf 'source  ~/ironcode-git-enhancements/bash_profile-mods.bash' >> ~/.bash_profile;
+printf "\nsource  ~/ironcode-git-enhancements/bash_profile-mods.bash\n" >> ~/.bash_profile;
 
 # Add line to ~/.gitconfig to load our gitconfig settings
-printf "\n[include]\n\tpath = ~/ironcode-git-enhancements/gitconfig" >> ~/.gitconfig
+printf "\n[include]\n\tpath = ~/ironcode-git-enhancements/gitconfig\n" >> ~/.gitconfig
